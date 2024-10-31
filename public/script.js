b74d6211bb18b25f232932d3fa8fe6f33975683b
jalal <fatouaki.jalal@gmail.com>
2024-10-31 15:24:34 -0400

    edit some things

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const stringurl = 'http://localhost:5001/';
 document.addEventListener('DOMContentLoaded', function () {
     // 'http://localhost:5001/';
     
-    const clientId = 'b758f477560242af9bd36645de2f7d0f';  // Replace with your Spotify Client ID
+    const clientId = config.clientId;  // Replace with your Spotify Client ID
     const redirectUri = `${stringurl}callback`;
     const scopes = 'playlist-read-private playlist-modify-public playlist-modify-private';
 
